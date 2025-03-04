bdb723c71954cf344f04aa65e000de3f00382859
sergi-laurent <sergilaurents@gmail.com>
2025-03-04 14:46:17 +0100

    FEATURE ✨ : Create Group Model & Seed Database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Book;
+use App\Models\Group;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory(count:10)->create();
         Book::factory(count:10)->create();
+        Group::factory(count:5)->create();
 
 
     }
