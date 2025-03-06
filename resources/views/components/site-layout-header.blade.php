badbb354c56e153b5176dd2b5344a4176678bf88
sergi-laurent <sergilaurents@gmail.com>
2025-03-06 03:53:05 +0100

    DESIGN 🎨 : Change brand colour from purple to blue
    This change in colours helps me distinguish this project from Nico's TucTuc blog project

--- resources/views/components/site-layout-header.blade.php
+++ resources/views/components/site-layout-header.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-purple-700 text-purple-50 mb-6">
+<div class="bg-blue-700 text-blue-50 mb-6">
         <div class="max-w-7xl m-auto flex justify-between items-center h-10">
             <div class="font-bold text-lg">BookLovers</div>
             <ul class="flex gap-x-4">
--- resources/views/site/books/index.blade.php
+++ resources/views/site/books/index.blade.php
@@ -3,7 +3,7 @@
 
     <ul class="grid grid-cols-3 gap-12 mt-12">
         @foreach($books as $book)
-            <li class="p-2 border-t border-t-black hover:bg-purple-50">
+            <li class="p-2 border-t border-t-black hover:bg-blue-50">
                 <a href="/books/{{$book->id}}">
                     <h3 class="font-bold text-2xl">{{$book->title}}</h3>
                     <p class="line-clamp-2">By {{$book->author}}</p>
