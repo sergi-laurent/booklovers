7e60c4e3320c1d03a95c3f2d9a94d01aa5014491
sergi-laurent <sergilaurents@gmail.com>
2025-03-06 03:55:44 +0100

    DESIGN 🎨 : Add SiteLayout component to the Show for books

--- resources/views/site/books/show.blade.php
+++ resources/views/site/books/show.blade.php
@@ -1,3 +1,5 @@
-<h1 class="font-bold text-2xl">{{$book->title}}</h1>
+<x-site-layout>
+    <h1 class="font-bold text-2xl">{{$book->title}}</h1>
 
-<p class="mt-4">{{$book->summary}}<p>
+    <p class="mt-4">{{$book->summary}}<p>
+</x-site-layout>
