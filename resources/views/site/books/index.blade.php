8c2645b8e57f9025da4fa82d4370eac84ab42668
sergi-laurent <sergilaurents@gmail.com>
2025-03-06 03:50:36 +0100

    DESIGN 🎨 : Book index Layout as a Gallery

--- resources/views/site/books/index.blade.php
+++ resources/views/site/books/index.blade.php
@@ -1,8 +1,14 @@
-<h1 class="font-bold text-2xl">This is the list of books</h1>
 <x-site-layout>
+    <h1 class="font-bold text-2xl">This is the list of books</h1>
 
-<ul>
-    @foreach($books as $book)
-        <li><a href="/books/{{$book->id}}">{{$book->title}}</li>
-    @endforeach
-</ul></x-site-layout>
+    <ul class="grid grid-cols-3 gap-12 mt-12">
+        @foreach($books as $book)
+            <li class="p-2 border-t border-t-black hover:bg-purple-50">
+                <a href="/books/{{$book->id}}">
+                    <h3 class="font-bold text-2xl">{{$book->title}}</h3>
+                    <p class="line-clamp-2">By {{$book->author}}</p>
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</x-site-layout>
