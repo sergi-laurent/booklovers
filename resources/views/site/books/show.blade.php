f37170039b68d403c26eed86c91e13e1d4af659b
sergi-laurent <sergilaurents@gmail.com>
2025-03-06 17:04:28 +0100

    REFACTOR 🔨 : Change repeated flex class in <p> tags

--- resources/views/site/books/show.blade.php
+++ resources/views/site/books/show.blade.php
@@ -12,8 +12,8 @@
 
     <h1 class="font-bold text-2xl">{{$book->title}}</h1>
     <div class="flex gap-1">
-        <p class="line-clamp-2 mt-2 flex">By 
-        <p class="line-clamp-2 mt-2 flex font-semibold">{{$book->author}}</p>
+        <p class="line-clamp-2 mt-2">By 
+        <p class="line-clamp-2 mt-2 font-semibold">{{$book->author}}</p>
     </div>
 
     <p class="mt-4">{{$book->summary}}<p>
