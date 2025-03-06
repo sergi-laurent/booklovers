73a7eb450b142abcf4db134810923d48c1ef693b
sergi-laurent <sergilaurents@gmail.com>
2025-03-06 03:48:43 +0100

    FEATURE ✨ : Add a SiteLayout Component and use it in the books index

--- resources/views/site/books/index.blade.php
+++ resources/views/site/books/index.blade.php
@@ -1,7 +1,8 @@
 <h1 class="font-bold text-2xl">This is the list of books</h1>
+<x-site-layout>
 
 <ul>
     @foreach($books as $book)
         <li><a href="/books/{{$book->id}}">{{$book->title}}</li>
     @endforeach
-</ul>
+</ul></x-site-layout>
