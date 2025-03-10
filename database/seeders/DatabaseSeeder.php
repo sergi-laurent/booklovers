b00e2f34e0bca15e7352f7afd26ca412ba078b93
sergi-laurent <sergilaurents@gmail.com>
2025-03-10 09:32:19 +0100

    FEATURE ✨ : Attaching Books to users -> Seeding the pivot table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Book;
 use App\Models\Group;
 use App\Models\Wishlist;
+use Illuminate\Support\Facades\Hash;
 
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -20,10 +21,29 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory(count:10)->create();
-        Book::factory(count:10)->create();
+        $my_user = User::create([
+            'name'=>'Sergi Laurent',
+            'email'=>'sergilaurents@gmail.com',
+            'password'=>Hash::make('password'),
+            ]);
+
+        $users = User::factory(count:10)->create();
+        $books = Book::factory(count:20)->create();
         Group::factory(count:5)->create();
         Wishlist::factory(count:10)->create();
+
+        //Add books to users
+       foreach($users as $user){
+        $random_number = random_int(1,10);
+        $books = Book::inRandomOrder()->take($random_number)->get();
+        $user->books()->attach($books);
+       }
+
+       //add books to My own User
+       $random_number = random_int(1,10);
+       $books = Book::inRandomOrder()->take($random_number)->get();
+       $my_user->books()->attach($books);
+
 
 
 
