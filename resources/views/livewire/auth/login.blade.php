36d609b15e394b326f0e319e669d0b42abae8154
sergi-laurent <sergilaurents@gmail.com>
2025-03-12 05:33:06 +0100

    FEATURE ✨ : Redirect to Groups Index after Login instead of Home Page

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -40,7 +40,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
         RateLimiter::clear($this->throttleKey());
         Session::regenerate();
 
-        $this->redirectIntended(default: route('home', absolute: false), navigate: true);
+        $this->redirectIntended(default: route('groups.index', absolute: false), navigate: true);
     }
 
     /**
