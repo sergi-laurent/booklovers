4e21d2ce1a0cb54cbc2cb1dafd56a729b0c16f9e
sergi-laurent <sergilaurents@gmail.com>
2025-03-12 05:32:20 +0100

    FEATURE ✨ : Link to Home Page by clicking the Logo in the Site Header

--- resources/views/components/site-layout-header.blade.php
+++ resources/views/components/site-layout-header.blade.php
@@ -1,6 +1,8 @@
 <div class="bg-blue-700 text-blue-50 mb-6">
         <div class="max-w-7xl m-auto flex justify-between items-center h-10">
-            <div class="font-bold text-lg">BookLovers</div>
+            <div>
+                <a href="{{ route('home') }}" class="font-bold text-2xl">BookLovers</a>
+            </div>
             <ul class="flex gap-x-4">
                 @foreach($menu as $label => $url)
                     <li class="hover:font-semibold hover:text-white"><a href="{{$url}}">{{$label}}</a></li>
