64bb6ab8e368a49364cdc845e8d20b9cc32603d9
sergi-laurent <sergilaurents@gmail.com>
2025-03-12 05:37:00 +0100

    FEATURE ✨ : Simplifying Group creation -> No description required

--- resources/views/site/groups/create.blade.php
+++ resources/views/site/groups/create.blade.php
@@ -9,8 +9,6 @@
 
         <x-form-input name='name' label='Group name' placeholder='Write the name of the group here'/>
 
-        <x-form-text-area name='description' label='Group description' rows='3' placeholder='Write a description for this group here'/>
-
     </x-form>
 
 </x-site-layout>
