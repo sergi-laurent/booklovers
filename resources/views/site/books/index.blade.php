13eede899a11cb626d9924e0da99222309f7411d
sergi-laurent <sergilaurents@gmail.com>
2025-03-12 05:36:16 +0100

    DESIGN 🎨 : Changing Buttons Design and Header of Books Index

--- resources/views/site/books/index.blade.php
+++ resources/views/site/books/index.blade.php
@@ -14,13 +14,21 @@
     @endif
 
 
-    <h1 class="font-bold text-3xl text-gray-600 mt-12">Explore All Books</h1>
+    <div class="px-4 mt-20">
+        <div class="flex">
+            <h3 class="text-gray-600 text-3xl font-bold">Explore All Books</h3>
+            <h3 class="text-gray-500 text-3xl font-semibold mx-2">- {{$books->count()}} Best Sellers</h3>
+        </div>
 
-    <div class="text-right">
-		<a href="{{route('books.create')}}" class="bg-blue-500 text-blue-50 uppercase p-2 hover:font-semibold rounded-sm">Create Book</a> 
-	</div>
+        <div class="flex justify-end pl-6">
+            <a href="{{route('books.create')}}" class="text-center bg-purple-500 text-purple-50 uppercase p-2 hover:font-semibold rounded-4xl">Create Book</a> 
+        </div>
+    </div>
 
-    <ul class="grid grid-cols-3 gap-8 mt-12">
+
+
+
+    <ul class="grid grid-cols-3 gap-8 mt-4">
         @foreach($books as $book)
             <li class="p-4 bg-gray-100 hover:bg-blue-50 flex flex-col justify-between h-full rounded-xl">
                 <a href="{{route('books.show', $book)}}">
@@ -31,27 +39,30 @@
                     </div>     
                 </a>
 
-                <div class="p-4 bg-gray-300 rounded-xl mt-6 mb-8">
-                    <p class="text-red-500">Book image HERE</p>
+                <!-- Push the image div to the bottom -->
+                <div class="flex-grow"></div>
+
+                <div class="p-4 bg-gray-300 rounded-xl mt-6">
+                    <p class="text-red-500 h-50">Book image HERE</p>
                 </div>
 
                 @if(auth()->user())
                     @if(auth()->user()->books->contains($book->id))
-                        <div class="bg-gray-300 text-white uppercase p-2 mt-4 text-center rounded hover:font-semibold w-full">
+                        <div class="bg-gray-300 text-white uppercase p-2 mt-2 text-center rounded hover:font-semibold w-full">
                             <p>You already have this book</p>
                         </div>
                         
                     @else
                         <form action="{{ route('library.store', $book->id) }}" method="POST">
                             @csrf
-                            <button type="submit" class="bg-blue-500 text-white uppercase p-2 mt-4 text-center rounded hover:bg-blue-600 hover:font-semibold w-full">
+                            <button type="submit" class="bg-blue-500 text-white uppercase p-2 mt-2 text-center rounded hover:bg-blue-600 hover:font-semibold w-full">
                                 Add to Library
                             </button>
                         </form>
                     @endif
 
                     @if(auth()->user()->wishlist->books->contains($book->id))
-                        <div class="bg-gray-300 text-white uppercase p-2 mt-4 text-center rounded hover:font-semibold w-full">
+                        <div class="bg-gray-300 text-white uppercase p-2 mt-2 text-center rounded hover:font-semibold w-full">
                             <p>You already have this book on your wishlist</p>
                         </div>
                         
@@ -66,7 +77,7 @@
                 @else
                     <form action="{{ route('library.store', $book->id) }}" method="POST">
                         @csrf
-                        <button type="submit" class="bg-blue-500 text-white uppercase p-2 mt-4 text-center rounded hover:bg-blue-600 hover:font-semibold w-full">
+                        <button type="submit" class="bg-blue-500 text-white uppercase p-2 mt-2 text-center rounded hover:bg-blue-600 hover:font-semibold w-full">
                             Add to Library
                         </button>
                     </form>
