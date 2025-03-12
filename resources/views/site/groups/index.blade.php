6a8f0a791048e8b5c453b9b5a1d9dbf5c8898dbe
sergi-laurent <sergilaurents@gmail.com>
2025-03-12 05:39:04 +0100

    DESIGN 🎨 :  Change Design of 'Create New Group' and adding Nº of Members on each group

--- resources/views/site/groups/index.blade.php
+++ resources/views/site/groups/index.blade.php
@@ -6,22 +6,30 @@
 		<a href="{{route('groups.create')}}" class="w-full text-center bg-blue-700 text-purple-50 uppercase p-2 hover:font-semibold rounded-sm">Create New Group</a> 
 	</div>
 
+    <div class="bg-gray-300 px-8 py-4 rounded-4xl">
+        <div class="flex">
+            <a href="{{route('groups.create')}}" class="w-full text-center bg-purple-600 text-purple-50 uppercase p-2 hover:font-semibold rounded-2xl">+ Create New Group</a> 
+        </div>
+    </div>
+
+    
+
     @if(auth()->user())
         @if($groups->isEmpty())
             <div class="text-gray-500 font-bold text-2xl text-center">
             You don't have any groups yet
             </div>
         @else
-            <ul class="grid grid-cols-1 gap-12 mt-6">
+            <ul class="grid grid-cols-1 gap-4 mt-6">
                 @if($groups->isEmpty())
                 You don't have any groups yet
                 @else
                     @foreach($groups as $group)
-                        <li class=" hover:bg-blue-50">
+                        <li class="bg-gray-100 p-6 rounded-2xl hover:bg-blue-50">
                             <a href="{{route('groups.show', $group)}}">
                                 <h3 class="font-bold text-2xl">{{$group->name}}</h3>
                                 <div class="flex justify-between items-center">
-                                    <p class="line-clamp-2">{{$group->description}}</p>
+                                    <p class="text-gray-500 font-semibold line-clamp-2">Members ({{$group->users->count()}})</p>
                                     <div class="text-right flex justify-end gap-2">
                                         <a href="{{route('groups.edit', $group)}}" class="bg-blue-100 text-blue-500 uppercase p-2 hover:font-semibold rounded-sm">Edit Group</a>
                                         <form action="{{route('groups.destroy', $group)}}" method="post">
