11611b5c87156d61850576a15256ea4dadf50839
sergi-laurent <sergilaurents@gmail.com>
2025-03-13 13:23:47 +0100

    REFACTOR 🔨 : Restructuring Routes and adding Comments

--- routes/web.php
+++ routes/web.php
@@ -3,49 +3,36 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Private Area Frist - Then Public Area
 
-// Create and store routes go first than the show because if I have the show route first, 
-// then the route for create will be expecting a parameter
+// Private Area -> User Pages = Wishlist, Library and Group functionalities
 Route::middleware(['auth'])->group(function(){
+
+    // Books - Authenticated users can create and store books
     Route::get('/books/create', [\App\Http\Controllers\BookController::class, 'create'])->name('books.create');
     Route::post('/books', [\App\Http\Controllers\BookController::class, 'store'])->name('books.store');
-});
 
-Route::get('/books', [\App\Http\Controllers\BookController::class, 'index'])->name('books.index');
-Route::get('/books/{book}', [\App\Http\Controllers\BookController::class, 'show'])->name('books.show');
-
-
-
-Route::get('/groups',[\App\Http\Controllers\GroupController::class, 'index'])->name('groups.index');
-
-
-// Private Area -> User Pages = Wishlist and Library and Group functionalities
-Route::middleware(['auth'])->group(function(){
+    // Wishlist
     Route::get('/wishlist/{user}', [\App\Http\Controllers\WishlistController::class, 'show'])->name('wishlist.show');
     Route::post('/wishlist/{book}', [\App\Http\Controllers\WishlistController::class, 'store'])->name('wishlist.store');
     Route::delete('/wishlist/{book}', [\App\Http\Controllers\WishlistController::class, 'destroy'])->name('wishlist.destroy');
 
+    // Library
     Route::get('/library/{user}', [\App\Http\Controllers\LibraryController::class, 'show'])->name('library.show');
     Route::post('/library/{book}', [\App\Http\Controllers\LibraryController::class, 'store'])->name('library.store');
     Route::delete('/library/{book}', [\App\Http\Controllers\LibraryController::class, 'destroy'])->name('library.destroy');
 
-
-    Route::get('/groups/create',[\App\Http\Controllers\GroupController::class, 'create'])->name('groups.create');
     
+    // Group Membership
     Route::get('/groups/{group}/add-member', [\App\Http\Controllers\GroupMemberController::class, 'addMemberForm'])->name('groups.addMemberForm');
     Route::post('/groups/{group}/add-member', [\App\Http\Controllers\GroupMemberController::class, 'addMember'])->name('groups.addMember');
-    Route::delete('/groups/{group}/quit-group', [\App\Http\Controllers\GroupMemberController::class, 'quitGroup'])->name('groups.quitGroup');
 
+    Route::delete('/groups/{group}/quit-group', [\App\Http\Controllers\GroupMemberController::class, 'quitGroup'])->name('groups.quitGroup');
 
     Route::get('/groups/join-group', [\App\Http\Controllers\GroupMemberController::class, 'joinGroupForm'])->name('groups.joinGroupForm');
     Route::post('/groups/join-group', [\App\Http\Controllers\GroupMemberController::class, 'joinGroup'])->name('groups.joinGroup');
-    Route::get('/groups/{group}',[\App\Http\Controllers\GroupController::class, 'show'])->name('groups.show');
 
-    Route::post('/groups',[\App\Http\Controllers\GroupController::class, 'store'])->name('groups.store');
-    Route::get('/groups/{group}/edit',[\App\Http\Controllers\GroupController::class, 'edit'])->name('groups.edit');
-    Route::put('/groups/{group}',[\App\Http\Controllers\GroupController::class, 'update'])->name('groups.update');
-    Route::delete('/groups/{group}',[\App\Http\Controllers\GroupController::class, 'destroy'])->name('groups.destroy');  
+    
 });
 
 
@@ -60,6 +47,20 @@ Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function(){
 
 });
 
+// Home Page Route
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// I put the Private Routes first becase I need the Create and store routes in the Private Area (only logged in users can create and sotre a book)
+// If I have the Public Area before that, then I will have the book show before /create and because if I have the show route first, 
+// then the route for create will be expecting a parameter
+
+// Books - Public Area
+Route::get('/books', [\App\Http\Controllers\BookController::class, 'index'])->name('books.index');
+Route::get('/books/{book}', [\App\Http\Controllers\BookController::class, 'show'])->name('books.show');
+
+
+// Groups - Public Area
+Route::get('/groups',[\App\Http\Controllers\GroupController::class, 'index'])->name('groups.index');
 
 
 Route::view('dashboard', 'dashboard')
