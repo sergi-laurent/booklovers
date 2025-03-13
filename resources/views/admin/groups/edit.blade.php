c7acc4fbffdc2f6bfa1791556af6a330ebc4bca0
sergi-laurent <sergilaurents@gmail.com>
2025-03-13 11:16:15 +0100

    BUGFIX 🐛 : Updating Admin Group Edit Form Route!

--- resources/views/admin/groups/edit.blade.php
+++ resources/views/admin/groups/edit.blade.php
@@ -1,10 +1,10 @@
 <x-site-layout>
 
     <x-form 
-        :action="route('groups.update', $group)" 
+        :action="route('admin.groups.update', $group)" 
         method="put" 
         title='Update Group' 
-        :cancelurl="route('groups.show', $group)" 
+        :cancelurl="route('admin.groups.show', $group)" 
         submittext="Update Group">
 
         <x-form-input name='name' label='Group title' placeholder='Write the name here' :value='$group->name'/>        
